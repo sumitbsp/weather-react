0b0184a9559c8e35b6ddff704f71ee16fc77c60a
sumitbsp <sumit.bsp88@gmail.com>
2020-06-07 00:12:01 +0530

    Updates

---
+++ precache-manifest.8bfc40436e4d9ea77c69f704660a4216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2537a166173d33aa026015a3565593",
+    "url": "/sumitbsp/weather-react/index.html"
+  },
+  {
+    "revision": "bd80a3cba7ec97c524f9",
+    "url": "/sumitbsp/weather-react/static/css/main.31210768.chunk.css"
+  },
+  {
+    "revision": "bfb5e5b7c1f6714bb4ad",
+    "url": "/sumitbsp/weather-react/static/js/2.d6842111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sumitbsp/weather-react/static/js/2.d6842111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd80a3cba7ec97c524f9",
+    "url": "/sumitbsp/weather-react/static/js/main.c711d528.chunk.js"
+  },
+  {
+    "revision": "c158a4429ae8f142bbb7",
+    "url": "/sumitbsp/weather-react/static/js/runtime-main.578f2ee1.js"
+  }
+]);
